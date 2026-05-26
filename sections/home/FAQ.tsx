54a24b3c1aece32dab12e60a57e29c18105de703
Shivaji Tiwari <shivajitiwari@gmail.com>
2026-05-26 18:21:37 +0530

    fix: expand FAQ and ContactCTA to full width (max-w-7xl)

--- sections/home/FAQ.tsx
+++ sections/home/FAQ.tsx
@@ -14,7 +14,7 @@ export default function FAQ() {
 
   return (
     <section className="py-24">
-      <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <motion.div
           ref={ref}
           initial={{ opacity: 0, y: 20 }}
